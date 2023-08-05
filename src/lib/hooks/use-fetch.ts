d3ace63684924b2e2b703f021019201e7defe8e5
Lucas Richard Garcia Tinoco <lucas.tinoco@hotmail.com>
2023-08-05 12:30:37 -0300

    feat: ensure useFetch is called with correct values

--- src/lib/hooks/use-fetch.ts
+++ src/lib/hooks/use-fetch.ts
@@ -5,19 +5,21 @@ const default_url = process.env.SERVER_URL || 'http://127.0.0.1:5000'
 type UseFetchParams = {
   url?: string
   route?: string
-  params?: string
+  params?: Record<string, any>
 }
+
 export const useFetch = async ({
   url = default_url,
   route = '',
-  params = ''
+  params = {}
 }: UseFetchParams) => {
-  const requestUrl = `${url}/${route === '/' ? '' : '/' + route}${
-    params ? encodeURIComponent(params) : ''
-  }`
+  const cleanedUrl = url.endsWith('/') ? url.slice(0, -1) : url
+  const cleanedRoute = route.startsWith('/') ? route.slice(1) : route
+
+  const requestUrl = `${cleanedUrl}/${cleanedRoute}`
 
   try {
-    return await axios.get(requestUrl)
+    return await axios.get(requestUrl, { params })
   } catch (error: any) {
     throw new Error(`Request to ${requestUrl} failed: ${error.message}`)
   }
--- test/lib/hooks/use-fetch.spec.ts
+++ test/lib/hooks/use-fetch.spec.ts
@@ -5,18 +5,52 @@ import { useFetch } from '../../../src/lib/hooks/use-fetch'
 jest.mock('axios')
 const mockedAxios = axios as jest.Mocked<typeof axios>
 
+const defaultUrl = 'http://127.0.0.1:5000'
+
 describe('useFetch Hook', () => {
-  it("should call axios.get with the correct url when '/' route is provided", async () => {
+  afterEach(() => {
+    mockedAxios.get.mockClear()
+  })
+
+  test("should call axios.get with the correct url when '/' route is provided", async () => {
     // arrange
-    const mockRoute = '/'
-    const expectedUrl = 'http://127.0.0.1:5000/'
+    const route = '/'
+    const expectedUrl = `${defaultUrl}/`
 
     mockedAxios.get.mockResolvedValue({ data: {} })
 
     // act
-    renderHook(() => useFetch({ route: mockRoute }))
+    renderHook(() => useFetch({ route: route }))
 
     // assert
-    expect(mockedAxios.get).toHaveBeenCalledWith(expectedUrl)
+    expect(mockedAxios.get).toHaveBeenCalledWith(expectedUrl, { params: {} })
+  })
+
+  test('should call axios.get with the correct url when a valid route is provided', async () => {
+    // arrange
+    const route = '/valid_route'
+    const expectedUrl = `${defaultUrl}/valid_route`
+
+    mockedAxios.get.mockResolvedValue({ data: {} })
+
+    // act
+    renderHook(() => useFetch({ route: route }))
+
+    // assert
+    expect(mockedAxios.get).toHaveBeenCalledWith(expectedUrl, { params: {} })
+  })
+  test('should call axios.get with the correct url when a valid route with valid params is provided', async () => {
+    // arrange
+    const route = '/valid_route'
+    const params = { search: 'valid_search' }
+    const expectedUrl = `${defaultUrl}/valid_route`
+
+    mockedAxios.get.mockResolvedValue({ data: {} })
+
+    // act
+    renderHook(() => useFetch({ route: route, params: params }))
+
+    // assert
+    expect(mockedAxios.get).toHaveBeenCalledWith(expectedUrl, { params })
   })
 })
