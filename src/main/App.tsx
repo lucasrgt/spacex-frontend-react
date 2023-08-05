b109e059b25820529a576b2321e1103703a378d5
Lucas Richard Garcia Tinoco <lucas.tinoco@hotmail.com>
2023-08-05 11:07:49 -0300

    chore: setup project architecture

---
+++ src/main/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <main className="bg-gradient-to-tl from-spacegray-500 to-spacegray-600 min-h-screen h-full tracking-widest">
+        {' '}
+      </main>
+    </>
+  )
+}
+
+export default App
--- src/main/main.tsx
+++ src/main/main.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './main.css'
-import { StateProvider } from './context/redux/provider.tsx'
+import { StateProvider } from '../lib/context/redux/provider.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
